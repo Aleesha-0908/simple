b72bb0ff4380e83477e9716823abeb675ad47849
Aleesha2002 <aleesha.lalit@gmail.com>
2023-06-24 00:25:03 +0530

    first pr

--- Backend/Server.js
+++ Backend/Server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 // });
 
 app.use("/api/user", userRoutes);
-
+app.use("/api/footer", footerRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000, console.log(`Server has started PORT`.yellow.bold));
